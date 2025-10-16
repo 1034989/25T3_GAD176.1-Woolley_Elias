56bb0f84e8c8d2c82ed534196978cbb1f4b07f34
Elias_Lab2.102_7001391 <1034989@student.sae.edu.au>
2025-10-16 17:00:50 +1100

    Week 5 class push
    Added a starter package with character controllers and an environment
    Added some pseudocode to AI scripts

---
+++ Assets/AI/AIScripts/AIBehaviour.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class AIBehaviour : MonoBehaviour
+{
+    //AI Behaviour
+    /// This script contains the information on how the AI patrols, hunts and attacks
+    /// Patrol
+    /// Hunt
+    /// Attack
+}
